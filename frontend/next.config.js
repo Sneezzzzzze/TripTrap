984c37e4a165afca8d6e9e4c2431800038cecce9
Sneezzzzzze <65070040@kmitl.ac.th>
2025-11-12 19:13:02 +0700

    Add amplify.yml and fix next.config for SSR

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,11 @@
-const amplifyAdapter = require('@aws-amplify/adapter-nextjs');
-
-console.log('Amplify Adapter exports:', Object.keys(amplifyAdapter));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
