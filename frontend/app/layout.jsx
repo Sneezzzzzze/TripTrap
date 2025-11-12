ac38c93fc7daf077a44d9267157caf9cb4ae2431
kasomsri <bess11234@hotmail.com>
2025-11-12 16:48:55 +0700

    fix: try to make Amplify to build on ssr

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,11 +1,14 @@
+import RootLayoutThatConfiguresAmplifyOnTheClient from "./components/ConfigureAmplifyOnTheClient";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <RootLayoutThatConfiguresAmplifyOnTheClient>
+        <body>
+          {children}
+        </body>
+      </RootLayoutThatConfiguresAmplifyOnTheClient>
     </html>
   );
 }
