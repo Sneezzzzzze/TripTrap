22d47052d2cebc11ef790cdeba6e0d7ce29666d1
Junbeom Kim <kimjunbeom1205@gmail.com>
2025-11-12 16:59:33 +0700

    Comment out Amplify configuration and exports

--- frontend/app/components/ConfigureAmplifyOnTheClient.tsx
+++ frontend/app/components/ConfigureAmplifyOnTheClient.tsx
@@ -1,16 +1,16 @@
-'use client';
+// 'use client';
 
-import outputs from '@/amplify_outputs.json';
-import { Amplify } from 'aws-amplify';
+// import outputs from '@/amplify_outputs.json';
+// import { Amplify } from 'aws-amplify';
 
-Amplify.configure(outputs, {
-    ssr: true // required when using Amplify with Next.js
-});
+// Amplify.configure(outputs, {
+//     ssr: true // required when using Amplify with Next.js
+// });
 
-export default function RootLayoutThatConfiguresAmplifyOnTheClient({
-    children
-}: {
-    children: React.ReactNode;
-}) {
-    return children;
-}
+// export default function RootLayoutThatConfiguresAmplifyOnTheClient({
+//     children
+// }: {
+//     children: React.ReactNode;
+// }) {
+//     return children;
+// }
