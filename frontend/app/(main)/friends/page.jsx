9ff305abb1e0f080e52a12f72cf6cd54aac6f6d1
kasomsri <bess11234@hotmail.com>
2025-11-14 09:48:25 +0700

    feat: revert to when don't fetch profile

--- frontend/app/(main)/friends/page.jsx
+++ frontend/app/(main)/friends/page.jsx
@@ -26,18 +26,7 @@ export default function FriendsPage() {
         { headers: { Authorization: `Bearer ${token}` } }
       );
 
-      let list = Array.isArray(res.data) ? res.data : [];
-      list = list.map(async (friend) => {
-        if (friend.image) {
-          const res = await axios.get(`/api/upload-url`, {
-            params: { key: friend.image },
-          });
-          if (res.status === 200 && res.data.url) {
-            friend.image = res.data.url
-          }
-        }
-        return friend
-      })
+      const list = Array.isArray(res.data) ? res.data : [];
       setFriends(list);
     } catch (err) {
       console.error("❌ reloadFriends error:", err);
@@ -270,8 +259,8 @@ export default function FriendsPage() {
                 >
                   <div className="flex items-center">
                     <img
-                      src={friend.image ?? "/profilepic/profile.jpg"}
-                      alt={"Picture of " + friend.username}
+                      src="/profilepic/profile.jpg"
+                      alt={friend.username}
                       className="w-12 h-12 rounded-full object-cover ring-2 ring-[#106681]/20"
                     />
                     <div className="ml-4">
